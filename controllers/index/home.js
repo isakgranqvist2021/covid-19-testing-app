a2a4602cec1681d9ab22bc54153df8fee0a905ee
isakgranqvist2021 <isakwebdev@gmail.com>
2021-08-11 18:09:08 +0200

    initial direction

--- controllers/index/home.js
+++ controllers/index/home.js
@@ -1,3 +1,17 @@
 export function get_home(req, res) {
     return res.render('index/home', {});
 }
+
+export function pick_test(req, res) {
+    if(req.params.test === 'rdt') {
+        return res.render('index/test-rdt', {
+
+        });
+    } else if(req.params.test === 'pcr') {
+        return res.render('index/test-pcr', {
+
+        });
+    } else {
+        return res.redirect('/')
+    }
+}
--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 const router = express.Router();
 
-import { get_home } from '../controllers/index/home';
+import { get_home, pick_test } from '../controllers/index/home';
 
 router.get('/', get_home);
+router.get('/pick-test/:test', pick_test);
 
 export default router;
