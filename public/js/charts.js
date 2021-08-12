ddf71dce34ec567f9bbcae1c13700485af70defd
isakgranqvist2021 <isakwebdev@gmail.com>
2021-08-13 01:40:54 +0200

    data stuff

--- public/js/charts.js
+++ public/js/charts.js
@@ -7,78 +7,170 @@
     Current month data by employment status. (Employee vs vendors/Guests)
 */
 
+const getDays = (days) =>
+    [...new Array(days)].map((i, idx) =>
+        moment().startOf("day").subtract(idx, "days")
+    );
+
+function checkIfBtwn(rawData, days) {
+    let values = [];
+    for (let i = 0; i < days.length; i++) {
+        values.push({
+            value: 0,
+            sday: days[i].startOf("day").toString(),
+            eday: days[i].endOf("day").toString(),
+        });
+    }
+
+    rawData.forEach((t) => {
+        values.forEach((v, i) => {
+            let isBtwn = moment(t.createdAt).isBetween(v.sday, v.eday);
+
+            if (isBtwn) {
+                v.value++;
+            }
+        });
+    });
+
+    return values.map((v) => v.value); // should be same length as days variable
+}
+
+function convertData(rawData, mode, dataset) {
+    let days = (() => {
+        if (mode === "dd") return getDays(1);
+        if (mode === "ww") return getDays(7);
+        if (mode === "mm") return getDays(30);
+    })();
+
+    let datapoints = (() => {
+        if (dataset === "status") {
+            return [
+                {
+                    label: "positive",
+                    datapoints: checkIfBtwn(rawData.positive, days),
+                },
+                {
+                    label: "negative",
+                    datapoints: checkIfBtwn(rawData.negative, days),
+                },
+            ];
+        } else if (dataset === "type") {
+            return [
+                {
+                    label: "PCR",
+                    datapoints: checkIfBtwn(rawData.pcr, days),
+                },
+                {
+                    label: "RDT",
+                    datapoints: checkIfBtwn(rawData.rdt, days),
+                },
+            ];
+        } else if (dataset === "entities") {
+            let n = 0;
+            let d = [];
+            let keys = [];
+
+            for (let k in rawData) {
+                n++;
+                keys.push(k);
+            }
+
+            for (let i = 0; i < n; i++) {
+                d.push({
+                    label: keys[i],
+                    datapoints: checkIfBtwn(rawData[keys[i]], days),
+                });
+            }
+
+            return d;
+        } else if (dataset === "employement") {
+            return [
+                {
+                    label: "employee",
+                    datapoints: (() => {
+                        return [65, 59, 80, 81, 56, 55, 40];
+                    })(),
+                },
+                {
+                    label: "vendor/guest",
+                    datapoints: (() => {
+                        return [65, 59, 80, 81, 56, 55, 40];
+                    })(),
+                },
+            ];
+        }
+    })();
+
+    return datapoints;
+}
+
 class CustomChart {
-    constructor(selector, label) {
+    constructor(selector, label, rawData) {
         this.label = label;
         this.element = document.querySelector(selector).getContext("2d");
         this.mode = "dd";
         this.chart = null;
-
-        document
-            .getElementById("graph-mode")
-            .addEventListener("change", (e) => {
-                this.mode = e.target.value;
-                this.chart.destroy();
-                this.init();
-            });
+        this.rawData = rawData;
+        this.data = convertData(this.rawData, this.mode, this.label);
     }
 
-    data() {
-        return [
-            {
-                label: this.label,
-                data: [65, 59, 80, 81, 56, 55, 40],
-                backgroundColor: [
-                    "rgba(255, 99, 132, 0.2)",
-                    "rgba(255, 159, 64, 0.2)",
-                    "rgba(255, 205, 86, 0.2)",
-                    "rgba(75, 192, 192, 0.2)",
-                    "rgba(54, 162, 235, 0.2)",
-                    "rgba(153, 102, 255, 0.2)",
-                    "rgba(201, 203, 207, 0.2)",
-                ],
-                borderColor: [
-                    "rgb(255, 99, 132)",
-                    "rgb(255, 159, 64)",
-                    "rgb(255, 205, 86)",
-                    "rgb(75, 192, 192)",
-                    "rgb(54, 162, 235)",
-                    "rgb(153, 102, 255)",
-                    "rgb(201, 203, 207)",
-                ],
-                borderWidth: 1,
-            },
-        ];
+    reload(withMode) {
+        this.mode = withMode;
+        this.data = convertData(this.rawData, this.mode, this.label);
+        this.chart.destroy();
+        this.init();
+    }
+
+    getData() {
+        let properties = {
+            borderWidth: 1,
+            backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(255, 159, 64, 0.2)",
+                "rgba(255, 205, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(201, 203, 207, 0.2)",
+            ],
+            borderColor: [
+                "rgb(255, 99, 132)",
+                "rgb(255, 159, 64)",
+                "rgb(255, 205, 86)",
+                "rgb(75, 192, 192)",
+                "rgb(54, 162, 235)",
+                "rgb(153, 102, 255)",
+                "rgb(201, 203, 207)",
+            ],
+        };
+
+        return this.data.map((d) => {
+            return {
+                ...properties,
+                label: d.label,
+                data: [...d.datapoints],
+            };
+        });
     }
 
     labels() {
         switch (this.mode) {
             case "dd":
-                return [moment(new Date()).format("dddd")];
+                return getDays(1).map((d) => moment(d).format("dddd"));
             case "ww":
-                return [...new Array(7)]
-                    .map((i, idx) =>
-                        moment().startOf("day").subtract(idx, "days")
-                    )
-                    .map((d) => moment(d).format("dddd"));
+                return getDays(7).map((d) => moment(d).format("dddd"));
             case "mm":
-                return [...new Array(30)]
-                    .map((i, idx) =>
-                        moment().startOf("day").subtract(idx, "days")
-                    )
-                    .map((d) => moment(d).format("dddd"));
+                return getDays(30).map((d) => moment(d).format("dddd"));
         }
     }
 
     init() {
-        const data = {
-            labels: this.labels(),
-            datasets: this.data(),
-        };
-
         this.chart = new Chart(this.element, {
             type: "bar",
-            data: data,
+            data: {
+                labels: this.labels(),
+                datasets: this.getData(),
+            },
             options: {
                 responsive: true,
                 stacked: false,
@@ -107,13 +199,27 @@ async function init() {
         },
     }).then((res) => res.json());
 
+    console.log(res);
+
     if (res.success) {
-        [
-            new CustomChart(".type-chart", "type"),
-            new CustomChart(".entities-chart", "entities"),
-            new CustomChart(".status-chart", "status"),
-            new CustomChart(".employement-chart", "employement"),
-        ].forEach((c) => c.init());
+        const charts = [
+            new CustomChart(".type-chart", "type", res.data.typeData),
+            new CustomChart(".entities-chart", "entities", res.data.entData),
+            new CustomChart(".status-chart", "status", res.data.statusData),
+            new CustomChart(
+                ".employement-chart",
+                "employement",
+                res.data.empData
+            ),
+        ];
+
+        document
+            .getElementById("graph-mode")
+            .addEventListener("change", (e) => {
+                charts.forEach((chart) => chart.reload(e.target.value));
+            });
+
+        charts.forEach((c) => c.init());
     }
 }
 
